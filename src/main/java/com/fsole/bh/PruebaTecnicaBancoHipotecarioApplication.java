dcd7f3744639c729f52d104d275a4adf33597f34
Francisco Solé <franciscosoleescande@gmail.com>
2025-09-27 18:10:27 -0300

    chore: set project

---
+++ src/main/java/com/fsole/bh/PruebaTecnicaBancoHipotecarioApplication.java
@@ -0,0 +1,13 @@
+package com.fsole.bh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaBancoHipotecarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaBancoHipotecarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsole/bh/PruebaTecnicaBancoHipotecarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsole.bh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaBancoHipotecarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
