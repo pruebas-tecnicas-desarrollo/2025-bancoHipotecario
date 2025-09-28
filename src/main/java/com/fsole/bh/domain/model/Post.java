755c7f378bb3b9281541ae6902b65e2875c492ba
Francisco Solé <franciscosoleescande@gmail.com>
2025-09-27 21:28:45 -0300

    refactor(model/Post): add User and List<Comment> properties

--- src/main/java/com/fsole/bh/domain/model/Post.java
+++ src/main/java/com/fsole/bh/domain/model/Post.java
@@ -2,10 +2,14 @@ package com.fsole.bh.domain.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Post {
     private Long id;
     private Long userId;
     private String title;
     private String body;
+    private User author;
+    private List<Comment> comments;
 }
