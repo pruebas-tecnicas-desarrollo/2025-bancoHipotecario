01410f218a1eeec32d3015410dd046723cdd5886
Francisco Solé <franciscosoleescande@gmail.com>
2025-09-27 23:03:30 -0300

    feat(GlobalExceptionHandler): add new exceptions

--- src/main/java/com/fsole/bh/infrastructure/adapter/in/rest/GlobalExceptionHandler.java
+++ src/main/java/com/fsole/bh/infrastructure/adapter/in/rest/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.fsole.bh.infrastructure.adapter.in.rest;
 
+import com.fsole.bh.domain.exception.ExternalServiceTimeoutException;
+import com.fsole.bh.domain.exception.InvalidRequestException;
 import com.fsole.bh.domain.exception.PostNotFoundException;
 import com.fsole.bh.domain.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -18,6 +20,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PostNotFoundException.class)
     public ResponseEntity<String> handlePostNotFound(PostNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ExternalServiceTimeoutException.class)
+    public ResponseEntity<String> handleTimeout(ExternalServiceTimeoutException ex) {
+        return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<String> handleTimeout(InvalidRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
 }
