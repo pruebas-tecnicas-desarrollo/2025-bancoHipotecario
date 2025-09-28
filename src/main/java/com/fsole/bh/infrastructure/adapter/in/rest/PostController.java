6dede9ddb4b60ae2ea8aeddd7096996464a6f0ab
Francisco Solé <franciscosoleescande@gmail.com>
2025-09-27 23:29:12 -0300

    refactor(PostController): add /posts request mapping

--- src/main/java/com/fsole/bh/infrastructure/adapter/in/rest/PostController.java
+++ src/main/java/com/fsole/bh/infrastructure/adapter/in/rest/PostController.java
@@ -4,14 +4,12 @@ import com.fsole.bh.domain.port.post.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/posts")
 @RequiredArgsConstructor
 @Slf4j
 public class PostController {
